66664627abd9ff22614c7ab74cd03d9a940abebf
Shekhar Gyanwali <saykharng@hotmail.com>
2020-03-30 20:29:35 +1100

    pdf merge beta version

--- main.js
+++ main.js
@@ -2,28 +2,34 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+app.allowRendererProcessReuse = true;
+
+
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 720,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
     }
   })
 
+  //mainWindow.webContents.openDevTools()
+
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+  mainWindow.setMenuBarVisibility(false);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
-
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
